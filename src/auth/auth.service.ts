a6d9df08ec3b55a286071165ef7833e6a5b65186
Dmitry Shneider <dmitryshneider@gmail.com>
2024-05-25 22:41:17 +0600

    Добавлен jwt модуль, добавлена отправка jwt токена

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,24 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private dataBaseService: DatabaseService) {}
+    constructor(
+        private dataBaseService: DatabaseService,
+        private jwtService: JwtService
+    ) {}
 
     async signIn(email: String, pass: String) {
         const user = await this.dataBaseService.getUserByEmail(email);
         if(user?.password !== pass) {
-            console.log(pass, user.password)
             throw new UnauthorizedException();
         }
 
-        const {password, ...result} = user;
+        const payload = {sub: user.id, name: user.first_name, surname: user.second_name, email: user.email, role: user.role}
 
-        return result;
+        return {
+            access_token: await this.jwtService.signAsync(payload)
+        };
     }
 }
---
+++ src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+    secret: 'Very very secret key'
+}
