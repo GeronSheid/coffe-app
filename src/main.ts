b9d79cf7513eebfb30050209f883214ece8822fd
Dmitry Shneider <dmitryshneider@gmail.com>
2024-05-30 16:56:38 +0600

    Настроена базовая авторизация + написана базовая форма авторизации

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // Разрешите доступ с этого происхождения
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // Разрешите передачу куки и авторизационных заголовков
+  });
   await app.listen(3000);
 }
 bootstrap();
